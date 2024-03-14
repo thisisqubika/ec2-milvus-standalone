70e5b91426ad85dcf6612b62db6752e3157dea2a
Miguel Elhaiek <miguelelhaiek@miguel-elhaiek.local>
2024-03-14 12:26:42 -0300

    Adding Testing instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,37 +12,37 @@ provider "aws" {
   region = var.region
 }
 
-# resource "aws_security_group" "milvus_sg" {
-#   name        = "milvus-sg"
-#   description = "Security group for Milvus deployment with Docker"
+resource "aws_security_group" "milvus_sg_testing" {
+  name        = "milvus-sg"
+  description = "Security group for Milvus deployment with Docker"
 
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 19530
-#     to_port     = 19530
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 19530
+    to_port     = 19530
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "milvus_instance" {
   ami                    = var.ami
   instance_type          = var.instance_type
   key_name               = var.key_name
-  security_groups        = [aws_security_group.milvus_sg.name]
+  security_groups        = [aws_security_group.milvus_sg_testing.name]
   user_data              = file("${path.module}/../create-milvus.sh")
   
 # tamano de disco
