22f09c29887a31be7bd9e4fcf4c43e9a42842f43
Miguel Elhaiek <miguelelhaiek@miguel-elhaiek.local>
2024-03-14 12:25:23 -0300

    Adding Testing instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,31 +12,31 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_security_group" "milvus_sg" {
-  name        = "milvus-sg"
-  description = "Security group for Milvus deployment with Docker"
+# resource "aws_security_group" "milvus_sg" {
+#   name        = "milvus-sg"
+#   description = "Security group for Milvus deployment with Docker"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port   = 19530
-    to_port     = 19530
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 19530
+#     to_port     = 19530
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 resource "aws_instance" "milvus_instance" {
   ami                    = var.ami
